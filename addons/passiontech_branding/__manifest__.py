effaf9ec5ccd1d470f656656daccdacfddf86a18
Mulutare <mulutare21@gmail.com>
2026-08-06 13:09:37 +0300

    Initialize PassionTech customized ERP

---
+++ addons/passiontech_branding/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "PassionTech Branding",
+    "version": "19.0.1.0.0",
+    "summary": "Passion Technology login branding",
+    "category": "Technical",
+    "author": "Passion Technology",
+    "license": "LGPL-3",
+    "depends": [
+        "web"
+    ],
+    "data": [
+        "views/templates.xml"
+    ],
+    "installable": True,
+    "application": False
+}
---
+++ addons/passiontech_core/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "PassionTech ERP Core",
+    "version": "19.0.1.0.0",
+    "summary": "Approved Passion Technology sales, inventory, and finance applications",
+    "category": "PassionTech",
+    "author": "Passion Technology",
+    "license": "LGPL-3",
+    "depends": [
+        "passiontech_branding",
+        "sale_management",
+        "stock",
+        "sale_stock",
+        "account"
+    ],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "auto_install": False
+}
