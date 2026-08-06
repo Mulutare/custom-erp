cc0a0e3173961a7b26051ecd9ae552c99339455c
Mulutare <mulutare21@gmail.com>
2026-08-06 13:45:23 +0300

    Add PassionTech roles and security policy

---
+++ addons/passiontech_security/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "PassionTech Security",
+    "version": "19.0.1.0.0",
+    "summary": "Passion Technology company roles and access control",
+    "category": "PassionTech",
+    "author": "Passion Technology",
+    "license": "LGPL-3",
+    "depends": [
+        "passiontech_core",
+        "auth_signup"
+    ],
+    "data": [
+        "security/groups.xml",
+        "data/security_settings.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
