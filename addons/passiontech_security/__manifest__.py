f6f2b0c63c6f22c3c69165ce9105ca297c131694
Mulutare <mulutare21@gmail.com>
2026-08-07 12:35:38 +0300

    Protect To-do routes by role

--- addons/passiontech_security/__manifest__.py
+++ addons/passiontech_security/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "PassionTech Security",
-    "version": "19.0.1.17.0",
+    "version": "19.0.1.18.0",
     "summary": "Passion Technology company roles and access control",
     "category": "PassionTech",
     "author": "Passion Technology",
