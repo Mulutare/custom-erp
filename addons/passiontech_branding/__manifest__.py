d5bc5f09033f99ac55341180a73148abeaf4c83b
Mulutare <mulutare21@gmail.com>
2026-08-08 20:18:10 +0300

    Remove remaining Odoo app branding

--- addons/passiontech_branding/__manifest__.py
+++ addons/passiontech_branding/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "PassionTech Branding",
-    "version": "19.0.2.0.0",
+    "version": "19.0.2.0.1",
     "summary": "PassionTech ERP white-label web branding",
     "category": "Technical",
     "author": "Passion Technology",
